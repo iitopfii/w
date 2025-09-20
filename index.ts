6ab7003647f1ab8a40fd8ed0b12c72f4ec62d827
iiTOPii <nampetjah@gmail.com>
2025-09-20 15:46:36 +0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,13 +1,19 @@
 import { Elysia } from "elysia";
-let data = [
-  {name:"top" , email:"test@gmail.com"},
-  {name:"top" , email:"test@gmail.com"},
-  {name:"top" , email:"test@gmail.com"},
-  {name:"top" , email:"test@gmail.com"},
-]
+import { cors } from '@elysiajs/cors';
+let data: Array<any> = [];
 const app = new Elysia()
-.post("/user" , (body) => {
-    return body;
+.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  exposeHeaders: ['Referrer-Policy']
+}))
+.post("/user" , (req) => {
+    data.push(req.body);
+    return {
+      success:true
+    };
 })
 .get("/user-list" ,()=>{
     return data;
